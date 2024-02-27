d416218882fd7b4ac255c185e1fc8f49013cc89b
IrynaKosenko <irakosenko79@gmail.com>
2024-02-27 21:02:39 +0200

    add while loop with sleeping every 5 sec.

--- script_chmod.sh
+++ script_chmod.sh
@@ -6,7 +6,10 @@ if [ ! -d "$dir" ]; then
     echo "Directory $dir doesnt exist"
 fi
 
-find $dir -type d -name "*" -exec chmod 770 {} \;  # this also changes permission for root folder
-
-
-find $dir -type f -name "*" -exec chmod 660 {} \;
+while [ true ]
+do
+    find $dir -type d -name "*" -exec chmod 770 {} \;  # this also changes permission for root folder
+    find $dir -type f -name "*" -exec chmod 660 {} \;
+    echo " Permissions has been changed."
+    sleep 5s
+done
